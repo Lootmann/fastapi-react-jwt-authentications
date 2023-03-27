ee157ec9137432a92e1b64e88ec7c6ae55fe82cc
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-28 04:36:46 +0900

    frontend: main, sidebar

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Sidebar() {
+  return (
+    <div className="w-44 fixed left-0 top-0 h-screen bg-slate-800">
+      <div className="flex flex-col pt-6 gap-4 items-center text-xl text-slate-200">
+        <p>Sidebar</p>
+        <p>Sidebar</p>
+        <p>Sidebar</p>
+      </div>
+    </div>
+  );
+}
