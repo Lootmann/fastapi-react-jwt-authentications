81bbc03879c59421c159ad22b3db7a726f2730bd
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-28 03:19:47 +0900

    backend: add react cors

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers.auths import router as auth_router
 from api.routers.users import router as user_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credential=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(auth_router)
